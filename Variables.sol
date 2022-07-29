3f8a8d5214fc93be56e8426c0faa5f0eebe366bb
Gabriel Sorrentino <contato.galileoacademy@gmail.com>
2022-07-29 10:44:42 -0300

    Variables

---
+++ Variables.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+contract Variables {
+    // State variables are stored on the blockchain.
+    string public text = "Hello";
+    uint256 public num = 123;
+
+    function doSomething() public {
+        // Local variables are not saved to the blockchain.
+        uint256 i = 456;
+
+        // Here are some global variables
+        uint256 previousBlockNumber = uint256(block.number - 1); // previous block number
+        bytes32 previousBlockHash = bytes32(blockhash(previousBlockNumber)); // hash of the given block when param 'blocknumber' is one of the 256 most recent blocks; otherwise returns zero
+        uint256 basefee = block.basefee; // current block's base fee.
+        uint256 chainid = block.chainid; // current chain id
+        address coinbase = block.coinbase; // current block miner's address
+        uint256 difficulty = block.difficulty; // current block difficulty
+        uint256 gasLimit = block.gaslimit; // current block gaslimit
+        uint256 currentBlockNumber = block.number; // current block number
+        uint256 timestamp = block.timestamp; // current block timestamp
+        uint256 gasLeft = gasleft(); // remaining gas
+        bytes calldata data = msg.data; // complete calldata
+        address sender = msg.sender; // address of the caller
+        bytes4 sig = msg.sig; // first four bytes of the calldata (i.e. function identifier)
+        uint256 gasPrice = tx.gasprice; // gas price of the transaction
+        address origin = tx.origin; // sender of the transaction (full call chain)
+    }
+}
